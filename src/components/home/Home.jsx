03ac444319b5831376b8b2dc9aad1b3dc7f6f92c
karthik <karthikkarthikstark@gmail.com>
2025-02-27 12:24:08 +0530

    new update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Home.css';
-import Me from '../../assets/avatar-1.svg';
+import Me from '../../assets/karthik.png.jpg';
 import HeaderSocials from './HeaderSocials';
 import ScrollDown from './ScrollDown';
 import Shapes from './Shapes';
@@ -9,13 +9,13 @@ const Home = () => {
     return (
         <section className="home container" id='home'>
             <div className="intro">
-                <img src={Me} alt="" className='home__img' width='120' />
-                <h1 className="home__name">Kathik</h1>
-                <span className="home__education">Web developer</span>
+                <img src={Me} alt="Karthik" className='home__img' />
+                <h1 className="home__name">Karthik</h1>
+                <span className="home__education">Web Developer</span>
 
                 <HeaderSocials />
 
-                <a href="#contact" className="btn"> Contact Me</a>
+                <a href="#contact" className="btn">Contact Me</a>
 
                 <ScrollDown />
             </div>
@@ -25,4 +25,4 @@ const Home = () => {
     )
 }
 
-export default Home
+export default Home;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Testimonials.css';
-import Image3 from '../../assets/avatar-3.svg'
-import Image4 from '../../assets/avatar-4.svg'
+import Image3 from '../../assets/avatar-3.svg';
+import Image4 from '../../assets/avatar-4.svg';
 
 import { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -10,51 +10,49 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 
 const Testimonials = () => {
-
 	const data = [
 		{
 			id: 1,
 			image: Image4,
-			title: "Kerren Ortlepp",
-			subtitle: "Founder @ Entrostat",
-			comment: "Greg worked at Entrostat as a full-stack developer. He was excellent and had a real eye for design. If there was ever a time where we had a complicated frontend or needed to know what the best CSS approach was for something - Greg was our goto man!",
+			title: "John Doe",
+			subtitle: "CEO @ TechCorp",
+			comment: "Karthik is a highly skilled developer with a keen eye for detail. His ability to create responsive and visually appealing designs is truly impressive!",
 		},
 		{
 			id: 2,
 			image: Image3,
-			title: "Thapelo Kekana",
-			subtitle: "Frontend Developer @ FNB",
-			comment: "I enjoy working with the theme and learn so much. You guys make the process fun and interesting. Good luck! 👍",
+			title: "Sarah Smith",
+			subtitle: "Project Manager @ WebSolutions",
+			comment: "Working with Karthik was a great experience! His expertise in React and frontend development helped us deliver an outstanding project on time.",
 		},
 	];
 
 	return (
 		<section className="testimonials container section">
-
 			<h2 className="section__title">Testimonials</h2>
 
-			<Swiper className="testimonial__container grid"
+			<Swiper
+				className="testimonial__container grid"
 				modules={[Pagination]}
 				spaceBetween={30}
 				slidesPerView={1}
 				loop={true}
 				grabCursor={true}
-				pagination={{ clickable: true }}>
-				{data.map(({ id, image, title, subtitle, comment }) => {
-					return (
-						<SwiperSlide className="testimonial__item" key={id}>
-							<div className="thumb">
-								<img src={image} alt="" />
-							</div>
-							<h3 className="testimonial__title">{title}</h3>
-							<span className="subtitle">{subtitle}</span>
-							<div className="comment">{comment}</div>
-						</SwiperSlide>
-					)
-				})}
+				pagination={{ clickable: true }}
+			>
+				{data.map(({ id, image, title, subtitle, comment }) => (
+					<SwiperSlide className="testimonial__item" key={id}>
+						<div className="thumb">
+							<img src={image} alt={title} />
+						</div>
+						<h3 className="testimonial__title">{title}</h3>
+						<span className="subtitle">{subtitle}</span>
+						<div className="comment">{comment}</div>
+					</SwiperSlide>
+				))}
 			</Swiper>
 		</section>
-	)
+	);
 }
 
-export default Testimonials
+export default Testimonials;
